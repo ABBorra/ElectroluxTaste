f9632973cfb04cfe0a258ee064da9ce50abf1b2b
AB_Borra <anjibabu@prestomall.com>
2023-05-29 16:43:14 +0800

    Created NavigationView with Navigation title.

--- ElectroluxTaste/ElectroluxTaste/Project/AppStructure/View/PhotoLandingView/PhotoLandingView.swift
+++ ElectroluxTaste/ElectroluxTaste/Project/AppStructure/View/PhotoLandingView/PhotoLandingView.swift
@@ -15,42 +15,45 @@ struct PhotoLandingView: View {
     @StateObject var photoListViewModel = PhotoListViewModel()
     
     var body: some View {
-        VStack {
-            ScrollView(.vertical, showsIndicators: true ) {
-                LazyVGrid(columns: Array(repeating: GridItem(.flexible(),
-                                                             spacing: 4), count: 3), spacing: 20) {
-                    
-                    ForEach(0..<(photoListViewModel.photoGallery?.count ?? 0), id: \.self) { index in
-                        VStack{
-                            if let photo = photoListViewModel.photoGallery {
-                                
-                                PhotoDataView(photoDetails: photo[index])
-                                    .frame(height: 200)
-                                    .frame(width: 150)
-                                
-                                    .contentShape(Path(CGRect(x: 0, y: 0, width: 150, height: 200)))
-                                    .onTapGesture {
-                                        selectedIndex = index
-                                        moveToDetailScreen = true
-                                    }
-
-                            } else {
-                                Color.clear
-                                    .frame(width: 0, height: 0)
+        NavigationView {
+            VStack {
+                ScrollView(.vertical, showsIndicators: true ) {
+                    LazyVGrid(columns: Array(repeating: GridItem(.flexible(),
+                                                                 spacing: 4), count: 3), spacing: 20) {
+                        
+                        ForEach(0..<(photoListViewModel.photoGallery?.count ?? 0), id: \.self) { index in
+                            VStack{
+                                if let photo = photoListViewModel.photoGallery {
+                                    
+                                    PhotoDataView(photoDetails: photo[index])
+                                        .frame(height: 200)
+                                        .frame(width: 150)
+                                    
+                                        .contentShape(Path(CGRect(x: 0, y: 0, width: 150, height: 200)))
+                                        .onTapGesture {
+                                            selectedIndex = index
+                                            moveToDetailScreen = true
+                                        }
+                                    
+                                } else {
+                                    Color.clear
+                                        .frame(width: 0, height: 0)
+                                }
                             }
                         }
+                        
                     }
-                    
+                                                                 .listStyle(.plain)
                 }
-                .listStyle(.plain)
             }
-        }
-        .onAppear {
-            if photoListViewModel.photoGallery?.count == 0 {
-                photoListViewModel.fetchData(){ }
+            .navigationTitle("Flickr Photos")
+            .onAppear {
+                if photoListViewModel.photoGallery?.count == 0 {
+                    photoListViewModel.fetchData(){ }
+                }
             }
+            .padding()
         }
-        .padding()
     }
 }
 
