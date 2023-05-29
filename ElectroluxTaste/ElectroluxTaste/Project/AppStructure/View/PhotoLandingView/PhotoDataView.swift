f5cea1a7606ed9c4f81a608f85862663d063ba21
AB_Borra <anjibabu@prestomall.com>
2023-05-29 16:18:58 +0800

    Photos Display layout implementation

--- ElectroluxTaste/ElectroluxTaste/Project/AppStructure/View/PhotoLandingView/PhotoDataView.swift
+++ ElectroluxTaste/ElectroluxTaste/Project/AppStructure/View/PhotoLandingView/PhotoDataView.swift
@@ -11,7 +11,38 @@ struct PhotoDataView: View {
     var photoDetails: Photo?
     
     var body: some View {
-        Text("DisplayPhotoView")
+        GeometryReader { proxy in
+            
+            VStack(alignment: .center, spacing: 0) {
+                
+                AsyncImage(url: URL(string: photoDetails?.urlM ?? ""),
+                           content: { image in
+                    image
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: proxy.size.width * 0.8)
+                        .frame(height: proxy.size.height * 0.8)
+                        .clipShape(RoundedRectangle(cornerRadius: 5.0))
+                        .clipped()
+                        
+                },  placeholder: {
+                    
+                })
+                
+                Text(photoDetails?.title ?? "")
+                    .lineLimit(2)
+                    .foregroundColor(Color(hue: 0.547, saturation: 0.859, brightness: 0.528))
+                    .font(.custom(SecretKeys.customFontName, size: 18))
+                    .multilineTextAlignment(.leading)
+                    
+                Text(photoDetails?.id ?? "")
+                    .lineLimit(1)
+                    .foregroundColor(.gray)
+                    .font(.custom(SecretKeys.customFontName, size: 15))
+                    .multilineTextAlignment(.leading)
+                Spacer()
+            }
+            .padding()
+        }
     }
 }
 
