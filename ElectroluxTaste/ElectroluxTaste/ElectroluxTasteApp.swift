1f354507ee0b9b42536d506512a26ffc431b995d
AB_Borra <anjibabu@prestomall.com>
2023-05-29 14:31:13 +0800

    - Created New Project - Initial View - Project Setup

---
+++ ElectroluxTaste/ElectroluxTaste/ElectroluxTasteApp.swift
@@ -0,0 +1,17 @@
+//
+//  ElectroluxTasteApp.swift
+//  ElectroluxTaste
+//
+//  Created by AB_Borra on 5/29/23.
+//
+
+import SwiftUI
+
+@main
+struct ElectroluxTasteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            PhotoLandingView()
+        }
+    }
+}
---
+++ ElectroluxTaste/ElectroluxTaste/PhotoLandingView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  ElectroluxTaste
+//
+//  Created by AB_Borra on 5/29/23.
+//
+
+import SwiftUI
+
+struct PhotoLandingView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct PhotoLandingView_Previews: PreviewProvider {
+    static var previews: some View {
+        PhotoLandingView()
+    }
+}
