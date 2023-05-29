66aff9ab94fae5353763b98d2a7bba484485fb11
AB_Borra <anjibabu@prestomall.com>
2023-05-29 16:16:39 +0800

    - CreatedPhoto display View - Pdxproj

---
+++ ElectroluxTaste/ElectroluxTaste/Project/AppStructure/View/PhotoLandingView/PhotoDataView.swift
@@ -0,0 +1,22 @@
+//
+//  PhotoDataView.swift
+//  ElectroluxTaste
+//
+//  Created by AB_Borra on 5/29/23.
+//
+
+import SwiftUI
+
+struct PhotoDataView: View {
+    var photoDetails: Photo?
+    
+    var body: some View {
+        Text("DisplayPhotoView")
+    }
+}
+
+struct PhotoDataView_Previews: PreviewProvider {
+    static var previews: some View {
+        PhotoDataView()
+    }
+}
